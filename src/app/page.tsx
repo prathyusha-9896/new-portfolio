1a71a88e13e11f24293850a10cd9495eddb58bb5
prathyusha-9896 <jfsjd.mum.prathyusha@gmail.com>
2024-06-24 11:36:59 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
+import Mobilenav from "./components/Mobilenav"
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#fff2ee]">
+      <Navbar />
+      {/*<div className="lg:hidden block">
+      <Mobilenav />
+      </div>*/}
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        {/*<AchievementsSection />*/}
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
